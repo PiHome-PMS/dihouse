97865319ade9d07ceb7270e369210e89bfb8b519
namdang-fdp <namdangcoder@gmail.com>
2025-12-27 19:41:25 +0700

    chore: test ci/cd in frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
     return (
         <div>
-            <Button>Dev team</Button>
+            <Button>Test</Button>
         </div>
     );
 }
